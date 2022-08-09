48ace645f59ba58be97b2cab2227d665d9de9dc8
MBSonys <96136696+MBSonys@users.noreply.github.com>
2022-08-09 22:21:33 +0300

    Accounts templates added

--- Wheelers/settings.py
+++ Wheelers/settings.py
@@ -161,3 +161,11 @@ TINYMCE_DEFAULT_CONFIG = {
 
 
 LOGIN_REDIRECT_URL = '/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'mano_pastas@gmail.com'
+EMAIL_HOST_PASSWORD = 'VerySecret'
