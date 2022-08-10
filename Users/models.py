5cd478ee7227161be25cf5915e974dcb14b9a5c8
MBSonys <96136696+MBSonys@users.noreply.github.com>
2022-08-10 19:59:19 +0300

    Basic auth implemented

--- Users/models.py
+++ Users/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Seller(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     photo = models.ImageField(default="default.png", upload_to="profile_pics")
+
+    def __str__(self):
+        return self.user.username
--- Wheelers/settings.py
+++ Wheelers/settings.py
@@ -167,5 +167,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_POST = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mano_pastas@gmail.com'
+EMAIL_HOST_USER = 'zppython10@gmail.com'
 EMAIL_HOST_PASSWORD = 'VerySecret'
